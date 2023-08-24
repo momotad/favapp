a895cd4a44930919a471f4895c6ab4e5b9e3da2d
momotad <momota.d@outlook.jp>
2023-08-24 15:17:00 +0900

    newとcreateのルーティング、コントローラー設定

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,20 @@
 class AppsController < ApplicationController
   def index
   end
+
+  def new
+    @app = App.new
+  end
+
+  def create
+    @app = App.new(app_params)
+  end
+
+
+
+  private
+  def app_params
+    params.require(:app).permit(:image, :name, :content, :genre_id).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "apps#index"
-  resources :apps, only:[:index]
+  resources :apps, only:[:index, :new, :create]
 end
