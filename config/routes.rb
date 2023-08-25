dcafd4876e25022a71df2496fbb19ee518286d99
momotad <momota.d@outlook.jp>
2023-08-25 15:23:49 +0900

    editのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     collection do
       get 'list'
       get 'list/:id', to: 'apps#show', as: 'list_app'
+      get 'list/:id/edit', to: 'apps#edit', as: 'edit_list_app'
     end
   end
 
