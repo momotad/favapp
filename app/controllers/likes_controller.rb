27c9eec25fe161cbb8800a468f8fb72a46763092
momotad <momota.d@outlook.jp>
2023-08-27 14:05:17 +0900

    いいねボタンクリックを非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,28 @@
 class LikesController < ApplicationController
+  before_action :set_app
 
   def create
     like = current_user.likes.build(app_id: params[:id])
-    @app = App.find(like.app_id)
+    # @app = App.find(like.app_id)
     like.save
-    render template: "apps/show"
+    respond_to do |format|
+      format.js
+    end
+    # render template: "apps/show" renderで別コントローラーのアクションを実行する場合のコード
   end
 
   def destroy
     like = Like.find_by(app_id: params[:id], user_id: current_user.id)
-    @app = App.find(like.app_id)
+    # @app = App.find(like.app_id)
     like.destroy
-    render template: "apps/show"
+    respond_to do |format|
+      format.js
+    end
+    # render template: "apps/show"# render template: "apps/show" renderで別コントローラーのアクションを実行する場合のコード
+  end
+
+  def set_app
+    @app = App.find(params[:id])
   end
 
 end
