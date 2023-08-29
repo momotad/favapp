47251c004a50f27ae12541e486459cceeab01b92
momotad <momota.d@outlook.jp>
2023-08-29 12:37:59 +0900

    いいね数表示と不要な記述削除等

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,22 +3,18 @@ class LikesController < ApplicationController
 
   def create
     like = current_user.likes.build(app_id: params[:id])
-    # @app = App.find(like.app_id)
     like.save
     respond_to do |format|
       format.js
     end
-    # render template: "apps/show" renderで別コントローラーのアクションを実行する場合のコード
   end
 
   def destroy
     like = Like.find_by(app_id: params[:id], user_id: current_user.id)
-    # @app = App.find(like.app_id)
     like.destroy
     respond_to do |format|
       format.js
     end
-    # render template: "apps/show"# render template: "apps/show" renderで別コントローラーのアクションを実行する場合のコード
   end
 
   def set_app
