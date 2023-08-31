6a82f5e591740601a02440d84a2798073ac2264c
momotad <momota.d@outlook.jp>
2023-08-31 23:25:28 +0900

    ルーティング微修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  root "apps#index"
+  root to: "apps#index"
   resources :likes, only: [:create, :destroy]
   resources :comments, only: :create
   resources :apps do
