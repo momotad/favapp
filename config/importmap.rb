acd6ea172ba5f7630c31ab520368841e0c35dc77
momotad <momota.d@outlook.jp>
2023-09-01 16:17:19 +0900

    コメント追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
-# pin_all_from "app/javascript/channels", under: "channels"
+# pin_all_from "app/javascript/channels", under: "channels"　←こちらが今回エラー要因　一旦channels未実装状態に
