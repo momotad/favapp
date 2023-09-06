b238fa4f5a1b298562c00beaf22f2f255cfcabe2
momotad <momota.d@outlook.jp>
2023-09-06 11:37:35 +0900

    リダイレクト先変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -13,7 +13,7 @@ class LikesController < ApplicationController
   def destroy
     like = Like.find_by(app_id: params[:app_id], user_id: current_user.id)
     like.destroy
-    respond_to apps_path
+    respond_to app_path(@app.id)
     # respond_to do |format|
     #   format.js
     # end
